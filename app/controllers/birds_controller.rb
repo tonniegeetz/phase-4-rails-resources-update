8ed102f9d84f29d1ae06a047f2e28db871ba9fc2
tonniegeetz <tonniegeetz@gmail.com>
2024-07-29 15:54:50 +0300

    update and custom routes

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,4 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
@@ -18,14 +17,37 @@ class BirdsController < ApplicationController
     if bird
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render json: { error: 'Bird not found' }, status: :not_found
     end
   end
+
+  # PUT/PATCH /birds/:id
+
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird, status: :accepted
+    else
+      render json: { error: 'Bird not found' }, status: :not_found
+    end
+  end
+
+  # handling bird likes
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: 'Bird not found' }, status: :not_found
+    end
+  end
+  # A note on breaking convention: by creating this custom route, we are breaking the REST conventions we had been following up to this point. One alternate way to structure this kind of feature and keep our routes and controllers RESTful would be to create a new controller, such as Birds::LikesController, and add an update action in this controller. The creator of Rails, DHH, advocates for this approach for managing sub-resourcesLinks to an external site..
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
-
 end
